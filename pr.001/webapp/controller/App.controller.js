6644f51341e586687b070232fc093aaef0ac37eb
YEON SUNG LEE <racetam_noey@aspnc.com>
2023-01-31 02:01:42 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'raCetam-noey/master'.
    
    Changes to be committed:
    	new file:   pr.001/.gitignore
    	new file:   pr.001/README.md
    	new file:   pr.001/package.json
    	new file:   pr.001/ui5-local.yaml
    	new file:   pr.001/ui5-mock.yaml
    	new file:   pr.001/ui5.yaml
    	new file:   pr.001/webapp/Component.js
    	new file:   pr.001/webapp/controller/App.controller.js
    	new file:   pr.001/webapp/controller/Main.controller.js
    	new file:   pr.001/webapp/css/style.css
    	new file:   pr.001/webapp/i18n/i18n.properties
    	new file:   pr.001/webapp/index.html
    	new file:   pr.001/webapp/localService/metadata.xml
    	new file:   pr.001/webapp/manifest.json
    	new file:   pr.001/webapp/model/models.js
    	new file:   pr.001/webapp/test/flpSandbox.html
    	new file:   pr.001/webapp/test/flpSandbox.js
    	new file:   pr.001/webapp/test/initFlpSandbox.js
    	new file:   pr.001/webapp/test/integration/AllJourneys.js
    	new file:   pr.001/webapp/test/integration/NavigationJourney.js
    	new file:   pr.001/webapp/test/integration/arrangements/Startup.js
    	new file:   pr.001/webapp/test/integration/opaTests.qunit.html
    	new file:   pr.001/webapp/test/integration/opaTests.qunit.js
    	new file:   pr.001/webapp/test/integration/pages/App.js
    	new file:   pr.001/webapp/test/integration/pages/Main.js
    	new file:   pr.001/webapp/test/testsuite.qunit.html
    	new file:   pr.001/webapp/test/testsuite.qunit.js
    	new file:   pr.001/webapp/test/unit/AllTests.js
    	new file:   pr.001/webapp/test/unit/controller/Main.controller.js
    	new file:   pr.001/webapp/test/unit/unitTests.qunit.html
    	new file:   pr.001/webapp/test/unit/unitTests.qunit.js
    	new file:   pr.001/webapp/utils/locate-reuse-libs.js
    	new file:   pr.001/webapp/view/App.view.xml
    	new file:   pr.001/webapp/view/Main.view.xml
    
    Changes not staged for commit:
    	modified:   .vscode/launch.json
    	modified:   pr.001/package.json
    	modified:   pr.001/webapp/i18n/i18n.properties
    	modified:   pr.001/webapp/manifest.json
    
    Untracked files:
    	pr.001/package-lock.json
    	pr.001/ui5-deploy.yaml

---
+++ pr.001/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("pr.001.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
