15916d0de786f0fa9b8b3af149cab92de8f1a834
Yeon <87324544+raCetam-noey@users.noreply.github.com>
2023-01-31 18:22:59 +0900

    2

--- pr.001/webapp/controller/ListPage.controller.js
+++ pr.001/webapp/controller/ListPage.controller.js
@@ -1,10 +1,15 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(
-	Controller
-) {
-	"use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+    "use strict";
 
-	return Controller.extend("pr.001.controller.ListPage", {
-	});
+    return Controller.extend("pr.001.controller.ListPage", {
+
+        onInit: function () {},
+
+        navDetail: function (oEvent) {
+            var oTable = oEvent.getSource();
+            var oContext = oTable.getSelectedItem().getBindingContext();
+            this.getOwnerComponent().getRouter().navTo("ObjectPage", {key: oContext.getPath().substr(1)});
+        }
+
+    });
 });
