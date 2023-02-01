eb9ba7991f1159d5decc2376b9a1952264f9237b
YEON SUNG LEE <racetam_noey@aspnc.com>
2023-02-01 12:00:48 +0000

    .

--- pr.001/webapp/controller/ObjectPage.controller.js
+++ pr.001/webapp/controller/ObjectPage.controller.js
@@ -6,5 +6,20 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("pr.001.controller.ObjectPage", {
+		onInit: function() {
+
+            this.getOwnerComponent().getRouter().getRoute("ObjectPage").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function(oEvent) {
+            this._sKey = "/" + oEvent.getParameter("arguments").key;
+            this.getView().bindElement({
+                path: this._sKey
+            });
+        },
+        onNavBack: function() {
+            this.getOwnerComponent().getRouter().navTo("RouteMain", null);
+        },
+
 	});
 });
